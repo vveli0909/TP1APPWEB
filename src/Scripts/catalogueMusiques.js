c381a9f1534129f5d79d92d00274c81da7235662
2139594 <2139594@cegeplimoilou>
2024-09-17 18:05:37 -0400

    Initialisez Musique du fichier Json 7

---
+++ src/Scripts/catalogueMusiques.js
@@ -0,0 +1,50 @@
+export const  catalogueMusiques = [
+    {
+        id:1,
+        nom:"destin",
+        src:"https://www.goutemesdisques.com/uploads/tx_gmdchron/pi1/destin.jpg",
+        date: new Date("2019-03-22"),
+        genre:"RAP",
+        auteur:"Ninho",
+        prix : 2.99
+
+    },
+    {
+        id:2,
+        nom:"24K Magic",
+        src:"https://upload.wikimedia.org/wikipedia/en/2/2b/Bruno_Mars_-_24K_Magic_%28Official_Album_Cover%29.png",
+        date: new Date("2016-11-18"),
+        genre:"R&B",
+        auteur:"Bruno Mars",
+        prix : 2.99
+
+    },
+    {
+        id:3,
+        nom:"One more Time",
+        src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMiYmXxm17-z8K236vILNjqXX5IP-fp0elfA&s",
+        date: new Date("2021-03-12"),
+        genre:"Punk",
+        auteur:"Daft Punk",
+        prix : 2.99
+
+    },
+    {
+        id:4,
+        nom:"n'y pense plus",
+        src:"https://i.ytimg.com/vi/mUTsvbridvM/maxresdefault.jpg",
+        date: new Date("2024-06-08"),
+        genre:"R&B",
+        auteur:"Tayc",
+        prix : 2.99
+    },
+    {
+        id:5,
+        nom:"Ratpi WORLD",
+        src:"https://i.scdn.co/image/ab67616d0000b27378ceec4a5df1a69cf1248504",
+        date: new Date("2026-01-26"),
+        genre:"RAP",
+        auteur:"Booba",
+        prix : 2.99
+    }
+]
